fa0b72ab2b8803dd9df85efb1b3ca776f8901e7f
mikedeboer <info@mikedeboer.nl>
2010-09-14 17:22:36 +0200

    added mount usage example

---
+++ test/test_mount.js
@@ -0,0 +1,21 @@
+/*
+ * @package jsDAV
+ * @subpackage DAV
+ * @copyright Copyright (C) 2010 Mike de Boer. All rights reserved.
+ * @author Mike de Boer <mike AT ajax DOT org>
+ * @license http://github.com/mikedeboer/jsDAV/blob/master/LICENSE MIT License
+ */
+
+var Sys   = require("sys"),
+    Http  = require("http"),
+    jsDAV = require("./../lib/jsdav");
+
+jsDAV.debugMode = true;
+
+var server = Http.createServer(function(req, resp) {
+    Sys.puts("Incoming request in other handler...");
+});
+
+server.listen(8080, "127.0.0.1");
+
+jsDAV.mount(__dirname + "/assets", "test", server);
