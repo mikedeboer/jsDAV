08c93d82c613c07f344e84428ec5e08baeb74a63
Ruben Daniels <ruben@c9.io>
2012-04-11 20:39:47 +0200

    * Fixed c9 rename bug result

--- test/test_plugins.js
+++ test/test_plugins.js
@@ -2,44 +2,16 @@
  * @package jsDAV
  * @subpackage DAV
  * @copyright Copyright(c) 2011 Ajax.org B.V. <info AT ajax.org>
- * @author Ruben Daniels <ruben AT c9 DOT io>
+ * @author Mike de Boer <info AT mikedeboer DOT nl>
  * @license http://github.com/mikedeboer/jsDAV/blob/master/LICENSE MIT License
  */
 "use strict";
 
-var assert = require("assert");
-var jsDAV  = require("./../lib/jsdav");
-var jsDAV_Filelist_Plugin = require("./../lib/DAV/plugins/filelist");
+var jsDAV = require("./../lib/jsdav");
 
 jsDAV.debugMode = true;
 
-module.exports = {
-    timeout: 30000,
-
-    setUpSuite: function(next) {
-        this.plugin = new jsDAV_Filelist_Plugin({
-            addEventListener : function(){}
-        });
-        next();
-    },
-
-    tearDownSuite: function(next) {
-        
-        next();
-    },
-
-    "test retrieving a file list": function(next) {
-        this.plugin.doFilelist({path: "./"}, {}, function(err, out){
-            assert.ok(out.indexOf("test_filelist.js") > -1);
-            
-            next();
-        });
-    }
-};
-
-process.on("exit", function() {
-    if (module.exports.conn)
-        module.exports.conn.end();
-});
-
-!module.parent && require("./../node_modules/asyncjs/lib/test").testcase(module.exports).exec();
+jsDAV.createServer({
+    node: __dirname + "/assets",
+    plugins: ["auth", /*"browser", */"codesearch", "filelist", "filesearch", "locks", "mount", "temporaryfilefilter"]
+}, 8000);
