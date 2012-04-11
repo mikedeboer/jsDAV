765a50a424ef1511a91bed63e92d9c3469f637ff
Ruben Daniels <ruben@c9.io>
2012-04-11 20:35:20 +0200

    * Changed author

--- test/test_plugins.js
+++ test/test_plugins.js
@@ -2,16 +2,44 @@
  * @package jsDAV
  * @subpackage DAV
  * @copyright Copyright(c) 2011 Ajax.org B.V. <info AT ajax.org>
- * @author Mike de Boer <info AT mikedeboer DOT nl>
+ * @author Ruben Daniels <ruben AT c9 DOT io>
  * @license http://github.com/mikedeboer/jsDAV/blob/master/LICENSE MIT License
  */
 "use strict";
 
-var jsDAV = require("./../lib/jsdav");
+var assert = require("assert");
+var jsDAV  = require("./../lib/jsdav");
+var jsDAV_Filelist_Plugin = require("./../lib/DAV/plugins/filelist");
 
 jsDAV.debugMode = true;
 
-jsDAV.createServer({
-    node: __dirname + "/assets",
-    plugins: ["auth", /*"browser", */"codesearch", "filelist", "filesearch", "locks", "mount", "temporaryfilefilter"]
-}, 8000);
+module.exports = {
+    timeout: 30000,
+
+    setUpSuite: function(next) {
+        this.plugin = new jsDAV_Filelist_Plugin({
+            addEventListener : function(){}
+        });
+        next();
+    },
+
+    tearDownSuite: function(next) {
+        
+        next();
+    },
+
+    "test retrieving a file list": function(next) {
+        this.plugin.doFilelist({path: "./"}, {}, function(err, out){
+            assert.ok(out.indexOf("test_filelist.js") > -1);
+            
+            next();
+        });
+    }
+};
+
+process.on("exit", function() {
+    if (module.exports.conn)
+        module.exports.conn.end();
+});
+
+!module.parent && require("./../node_modules/asyncjs/lib/test").testcase(module.exports).exec();
