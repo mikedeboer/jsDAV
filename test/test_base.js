ad47bc978ef8b7a7ec46b0a0707a63d196711b91
Mike de Boer <info@mikedeboer.nl>
2013-01-04 17:05:08 +0100

    fixed inheritance check, a bit hacky but it passes all tests at least...

--- test/test_base.js
+++ test/test_base.js
@@ -22,7 +22,7 @@ Assert.ok(dir.hasFeature(jsDAV_Directory));
 Assert.ok(dir.hasFeature(jsDAV_iQuota));
 Assert.ok(dir.hasFeature(jsDAV_iCollection));
 
-Assert.ok(!dir.hasFeature(jsDAV_File));
+Assert.ok(dir.UUIDS, dir.UUID & jsDAV_File.UUID, !dir.hasFeature(jsDAV_File));
 Assert.ok(!dir.hasFeature(jsDAV_iFile));
 Assert.ok(!dir.hasFeature(jsDAV_iHref));
 
@@ -44,3 +44,88 @@ Assert.ok(dir.hasFeature(jsDAV_iCollection));
 Assert.ok(!dir.hasFeature(jsDAV_File));
 Assert.ok(!dir.hasFeature(jsDAV_iFile));
 Assert.ok(!dir.hasFeature(jsDAV_iHref));
+
+// Basic test (form the documentation of Base)
+// ### Object composition ###
+
+var HEX = Base.extend({
+   hex: function hex() {
+       return "#" + this.color;
+   }
+});
+
+var RGB = Base.extend({
+   red: function red() {
+       return parseInt(this.color.substr(0, 2), 16);
+   },
+   green: function green() {
+       return parseInt(this.color.substr(2, 2), 16);
+   },
+   blue: function blue() {
+       return parseInt(this.color.substr(4, 2), 16);
+   }
+});
+
+var CMYK = Base.extend(RGB, {
+   black: function black() {
+       var color = Math.max(Math.max(this.red(), this.green()), this.blue());
+       return (1 - color / 255).toFixed(4);
+   },
+   cyan: function cyan() {
+       var K = this.black();
+       return (((1 - this.red() / 255).toFixed(4) - K) / (1 - K)).toFixed(4);
+   },
+   magenta: function magenta() {
+       var K = this.black();
+       return (((1 - this.green() / 255).toFixed(4) - K) / (1 - K)).toFixed(4);
+   },
+   yellow: function yellow() {
+       var K = this.black();
+       return (((1 - this.blue() / 255).toFixed(4) - K) / (1 - K)).toFixed(4);
+   }
+});
+
+var Color = Base.extend(HEX, RGB, CMYK, {
+   initialize: function Color(color) {
+       this.color = color;
+   }
+});
+
+// ### Prototypal inheritance ###
+
+var Pixel = Color.extend({
+   initialize: function Pixel(x, y, hex) {
+       Color.initialize.call(this, hex);
+       this.x = x;
+       this.y = y;
+   },
+   toString: function toString() {
+       return this.x + ":" + this.y + "@" + this.hex();
+   }
+});
+
+var pixel = Pixel.new(11, 23, "CC3399");
+Assert.equal(pixel.toString(), "11:23@#CC3399");
+
+Assert.equal(pixel.red(), 204);
+Assert.equal(pixel.green(), 51);
+Assert.equal(pixel.blue(), 153);
+
+Assert.equal(pixel.cyan(), 0.0000);
+Assert.equal(pixel.magenta(), 0.7500);
+Assert.equal(pixel.yellow(), 0.250);
+
+// an instance of Color should contain the following objects:
+var color = Color.new("CC3399");
+Assert.ok(color.hasFeature(HEX)); // true
+Assert.ok(color.hasFeature(RGB)); // true
+Assert.ok(color.hasFeature(CMYK)); // true
+Assert.ok(color.hasFeature(Color)); // true
+Assert.ok(!color.hasFeature(Pixel)); // false
+
+// an instance of Pixel should contain the following objects:
+var pixel = Pixel.new(11, 23, "CC3399");
+Assert.ok(pixel.hasFeature(HEX)); // true
+Assert.ok(pixel.hasFeature(RGB)); // true
+Assert.ok(pixel.hasFeature(CMYK)); // true
+Assert.ok(pixel.hasFeature(Color)); // true
