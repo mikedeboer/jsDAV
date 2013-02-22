87632a78b1945ffb50e0ea2b2e7dc8ee799e7126
Wouter Vroege <wouter@woutervroege.nl>
2013-02-22 19:01:06 +0100

    deleted unused lines of code
    Signed-off-by: Wouter Vroege <wouter@woutervroege.nl>

--- examples/addressbookserver.js
+++ examples/addressbookserver.js
@@ -35,6 +35,10 @@ var jsDAVACL_Plugin = require("./../lib/DAVACL/plugin");
 
 var Db = require("./../lib/shared/backends/" + DB_DRIVER);
 
+// Make sure this setting is turned on and reflect the root url for your WebDAV server.
+// This can be for example the root / or a complete path to your server script
+var baseUri = "/";
+
 // Arguments to be passed to the function that establishes a connection with the db
 var DB_ARGS = {};
 
@@ -54,7 +58,6 @@ Db.getConnection(DB_ARGS, function(err, conn) {
 
     // Setting up the directory tree
 
-
     jsDAV.createServer({
         node: nodes,
         baseUri: baseUri,
@@ -62,45 +65,5 @@ Db.getConnection(DB_ARGS, function(err, conn) {
         realm: "jsDAV",
         plugins: [jsDAV_Auth_Plugin, jsDAV_Browser_Plugin, jsCardDAV_Plugin, jsDAVACL_Plugin]
     }, 8000);
-    	
-/*
 
-
-*/
 });
-
-// Make sure this setting is turned on and reflect the root url for your WebDAV server.
-// This can be for example the root / or a complete path to your server script
-var baseUri = "/";
-
-// set it up for demo use:
-
-/*
-new Db(DB_ARGS, function(err, db) {
-    if (err)
-        throw(err);
-       
-    console.log("mongodb initted!")
-    Db = db;
-
-    // Backends
-    var authBackend = jsDAV_Auth_Backend.new(db);
-    var principalBackend = jsDAVACL_PrincipalBackend.new(db);
-    var carddavBackend = jsCardDAV_Backend.new(db);
-    
-    // Setting up the directory tree
-    var nodes = [
-        jsDAVACL_PrincipalCollection.new(principalBackend),
-        jsCardDAV_AddressBookRoot.new(principalBackend, carddavBackend)
-    ];
-    
-    jsDAV.createServer({
-        node: nodes,
-        baseUri: baseUri,
-        authBackend: authBackend,
-        realm: "jsDAV",
-        plugins: [jsDAV_Auth_Plugin, jsDAV_Browser_Plugin, jsCardDAV_Plugin, jsDAVACL_Plugin]
-    }, 8000);
-});
-
-*/
