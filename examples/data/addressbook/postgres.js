59866f2a53368c42fa98048fd05815b4c98618d6
Daniel Laxar <i.dlaxar@gmail.com>
2014-07-08 21:49:48 +0200

    Fixing code style

--- examples/data/addressbook/postgres.js
+++ examples/data/addressbook/postgres.js
@@ -1,3 +1,10 @@
+/*
+ * @package jsDAV
+ * @subpackage DAV
+ * @copyright Copyright(c) 2011 Ajax.org B.V. <info AT ajax.org>
+ * @author Daniel Laxar <info AT mikedeboer DOT nl>
+ * @license http://github.com/mikedeboer/jsDAV/blob/master/LICENSE MIT License
+ */
 "use strict";
 
 var Async = require("asyncjs");
